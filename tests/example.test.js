b9f0dd53373402a55b1310ca83b0bf0cac7e62ec
aryan2574 <ayush.aryan94@yahoo.com>
2022-07-17 18:18:29 +0530

    Added various sample tests for the website

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,16 +1,46 @@
-const puppeteer = require('puppeteer');
+//Getting content from the webpage
+// Usage of assertions
 
-describe('My Puppeteer Test', () => {
-  it('should launch the browser', async function () {
-    const browser = await puppeteer.launch({   //Launch the browser
-      headless: false,
-      slowMo: 100,
-      devtools: false
+const puppeteer = require('puppeteer')
+const expect = require('chai').expect
+
+describe('Puppeteer Test ', () => {
+  it('should launch the browser',async function () {
+    //Loading
+    const browser = await puppeteer.launch({
+      headless:false,
+      slowMo:0,
+      devtools:false
     })
-    const page = await browser.newPage()   //Open Page from browser
-    await page.goto('https://devexpress.github.io/testcafe/example/')  //Will go to this website
-    await page.waitForSelector('h1')    //Search for the selector. If not found, will throw error
+    const page = await browser.newPage()
+    await page.goto('https://example.com/')
+    await page.waitForSelector('h1')
+    await page.waitForXPath('//h1')   //We can search for selector in the form of Xpath
 
-    await browser.close()                                                        // Will close the browser
+    //Getting the content
+    const title = await page.title()
+    const url = await page.url()
+    const text = await page.$eval('p',e => e.textContent)    //$eval for single elements
+    const countOfP = await page.$$eval('p', e => e.length)   //$$eval for multiple elements
+
+    //Printing the content
+    console.log('Title of the page ' + title);
+    console.log('URL of the page ' + url)
+    console.log('Text Content in P tag : ' + text)
+    console.log('Number of P tags on website : ' + countOfP)
+
+    //Assertions using Chai
+    expect(title).to.be.a('string','Example Domain')
+    expect(url).to.include('example.com')
+    expect(text).to.be.a('string','This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission.')
+    expect(countOfP).to.equal(2)
+
+    await page.goto('http://zero.webappsecurity.com/');
+    await page.waitForSelector('#searchTerm')
+    await page.type('#searchTerm','Ayush Aryan',{delay:100})
+    await page.keyboard.press('Enter',{delay:10})   //Used to press button from keyboard
+
+    await page.waitForTimeout(1000)
+    await browser.close()
   })
 })
